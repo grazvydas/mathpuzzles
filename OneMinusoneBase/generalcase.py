591e0b9bbca5f45a877f7c5cb792f6d0fa26d2c0
Gražvydas Šemetulskis <grazwydas@gmail.com>
2016-02-18 16:32:53 +0200

    general random case

--- OneMinusoneBase/generalcase.py
+++ OneMinusoneBase/generalcase.py
@@ -2,20 +2,43 @@ import numpy as np
 from itertools import product
 
 n = 20
-a = []
-for i in range(n):
-    a.append(i % 2 * 2 -1)  # taking first vector (1, 1, ..., 1) finds only 4 vectors
-A = [a]
 
-allcases = product([-1, 1], repeat=n)
 
-for case in allcases:
-    if all(np.dot(np.array(A), case) == 0):
-        A.append(case)
-        print np.array(A).shape
-        print np.array(A)
+# straight forward 
+def straight_forward(n):
+    allcases = product([-1, 1], repeat=n)
+    a = []
+    for i in range(n):
+        a.append(i % 2 * 2 -1)  # taking first vector (1, 1, ..., 1) finds only 4 vectors
+    A = [a]
+    for case in allcases:
+        if all(np.dot(np.array(A), case) == 0):
+            A.append(case)
+            print(np.array(A).shape)
+            print(np.array(A))
+    print('We found %(no_perpendicular)s perpendicular vectors of dimension %(dimension)s' % \
+          {'no_perpendicular': len(A), 'dimension': str(n)})
+    print(np.array(A))
 
-print 'We found %(no_perpendicular)s perpendicular vectors of dimension %(dimension)s' % \
-      {'no_perpendicular': len(A),
-       'dimension': str(n)}
-print(np.array(A))
+
+def random_vector(n):
+    K = 1
+    a = [1]*n
+    A = [a]
+
+    while K < n:
+        new =  2*np.random.randint(2, size=n)-1
+        if all(np.dot(np.array(A), new) == 0):
+            A.append(new)
+            K += 1
+            print(np.array(A).shape)
+            print(np.array(A))
+
+    print('We found %(no_perpendicular)s perpendicular vectors of dimension %(dimension)s' % \
+         {'no_perpendicular': len(A), 'dimension': str(n)})
+    print(np.array(A))
+
+
+
+if __name__ == '__main__':
+    random_vector(28)
