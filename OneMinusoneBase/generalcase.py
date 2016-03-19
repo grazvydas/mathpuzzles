a161ed396c419a9746a77d7ac9f9f8a0bbdc1b7e
Gražvydas Šemetulskis <grazwydas@gmail.com>
2016-03-19 17:50:02 +0200

    Implementations of Linas suggestion variation.

--- OneMinusoneBase/generalcase.py
+++ OneMinusoneBase/generalcase.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import product
+import time
 
 n = 28
 
@@ -36,14 +37,60 @@ def random_vector(n):
             A.append(sample.copy())
             K += 1
             print(np.array(A).shape)
-            print(np.array(A))
+            #print(np.array(A))
 
     print('We found %(no_perpendicular)s perpendicular vectors of dimension %(dimension)s' % \
          {'no_perpendicular': len(A), 'dimension': str(n)})
     print(np.array(A))
 
+def random_vector_with_replacement(n):
+    K = 1
+    a = [1]*n
+    A = [a]
+    nr = int(n/2)
+    sample = np.array([1]*nr + [-1]*nr)
+
+    while K < n:
+        np.random.shuffle(sample)
+        #new =  2*np.random.randint(2, size=n)-1
+        where = np.flatnonzero(np.dot(np.array(A), sample))
+        if len(where)==0: 
+            A.append(sample.copy())
+            K += 1
+            print(np.array(A).shape)
+            #print(np.array(A))
+        elif len(where)==1:
+            A[where[0]] = sample.copy()
+            #print(np.array(A).shape)
+            #print(np.array(A))
+            
+    print('We found %(no_perpendicular)s perpendicular vectors of dimension %(dimension)s' % \
+         {'no_perpendicular': len(A), 'dimension': str(n)})
+    print(np.array(A))
+
+# count execution time
+def test_execution():
+    n = 20
+    NUM_OF_TRIALS = 5
+    start_time = time.time()
+    for i in range(NUM_OF_TRIALS):
+        print(" %s is started " % i)
+        random_vector_with_replacement(n)
+        print(" %s is done " % i)
+    v1_time = time.time() - start_time
+    start_time = time.time()
+    for i in range(NUM_OF_TRIALS):
+        print(" %s is started " % i)
+        random_vector(n)
+        print(" %s is done " % i)
+    v2_time = time.time() - start_time
+
+    print("--- with replacment version takes %s seconds ---" % v1_time) 
+    print("--- with no replacment version takes %s seconds ---" % v2_time) 
 
 
 if __name__ == '__main__':
-    random_vector(n)
     #straight_forward(n)
+    random_vector_with_replacement(n)
+    #random_vector(20)
+    #test_execution()
